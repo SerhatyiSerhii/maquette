fe57ecd0505c87a0dfdeb5f8464b23619d46f2ea
Serhii Serhatyi <serhatyi.serhii@gmail.com>
2021-02-20 19:02:27 +0200

    applyd click effect on burger

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -1,33 +1,54 @@
-$(function() {
-    //Smooth scroll to the film at Go-To menu
-    $('a.top-film').click(function(event) {
-        event.preventDefault();
-        //The Method preventDefault() of an object Event:
-        //This Method cancels the default behavior of
-        //an Event in a browser that is applied by a user
-        //to an element of a page.
+document.addEventListener('DOMContentLoaded', function() {
+    $(function() {
+        //Smooth scroll to the film at Go-To menu
+        $('a.top-film').on('click', function(event) {
+            event.preventDefault();
 
-        //In our case Event is a left click of a mouse.
-        //When a user clicks on a link with a class top-film
-        //the link will not work. By default a link should
-        //lead the user to a new page or to the specific part
-        //of the active page. But the defualt behavior is
-        //cancelled and nothing is going to happend.
-        //BTW: the Event "click" is still spreading on other
-        //elements in DOM-structure,
-        //but the Event does nothing.
+            var topLink = $(this).attr('href');
 
-        var topLink = $(this).attr('href');
+            $('html').animate({
+                scrollTop: $(topLink).offset().top
+            }, 300);
+        });
 
-        $('html').animate({
-            scrollTop: $(topLink).offset().top
-        }, 300);
-    });
+        //Hover on Go-To menu
+        $('.go-to').hover(function() {
+            $('.film-nav').finish().slideToggle(300);
+        });
 
-    //Hover on Go-To menu
-    $(".go-to").hover(function() {
-        $(".film-nav").finish().slideToggle(300);
-    });
+        if (window.matchMedia('(max-width: 767px)').matches) {
+
+            $("#nav-wrapper").on("click", function(event) {
+                event.stopPropagation();
+                $(".box-menu").finish().slideToggle(300);
+
+                $("span").toggleClass("pressed");
+            });
+
+            $(".film-nav a").on("click", function() {
+                $("span").toggleClass("pressed");
+                $(".box-menu").finish().slideToggle(300);
+            });
+
+            $(document).click(function() {
+                var arr = []
+
+                for(var i = 0; i < $(".box-menu > li > a").length; i++){
+                    arr.push($(".box-menu > li > a")[i]);
+                }
+
+                /*this part I don't like as I can't apply
+                FOR loop. If I apply FOR loop the click on
+                item in menu also hides the menu*/
+
+                if(event.target != arr[0] &&
+                    event.target != arr[1] &&
+                    event.target != arr[2] &&
+                    event.target != arr[3]) {
+                    $(".box-menu").hide();
+                    $("span").removeClass("pressed");
+                }
+            });
+        }
+    })
 });
-
-
