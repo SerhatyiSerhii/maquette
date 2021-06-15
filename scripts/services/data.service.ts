20004ed50e29c0546c76c4b58d4c2669ad464c8f
Serhii Serhatyi <serhatyi.serhii@gmail.com>
2021-06-15 18:31:27 +0300

    part 1: XMLHttpRequests at data.service

--- scripts/services/data.service.ts
+++ scripts/services/data.service.ts
@@ -1,17 +1,41 @@
 import { IAudio } from '../models/i-audio';
 import { IMovie } from '../models/i-movie';
-import { audioDataBase, dataBase } from './database';
+// import { audioDataBase, dataBase } from './database';
 
 export class DataService {
     getAllMovies(callback: (data: IMovie[]) => void): void {
-        setTimeout(() => {
-            callback(dataBase);
-        }, 300);
+        // setTimeout(() => {
+        //     callback(dataBase);
+        // }, 300);
+
+        const xml = new XMLHttpRequest();
+
+        xml.open('GET', 'http://localhost:8080/movies');
+
+        xml.responseType = 'json';
+
+        xml.send();
+
+        xml.onload = () => {
+            callback(xml.response);
+        }
     }
 
     getAudioSourceById(id: number, callback: (audio: IAudio) => void): void {
-        setTimeout(() => {
-            callback(audioDataBase.find(entity => entity.id === id));
-        }, 300);
+        // setTimeout(() => {
+        //     callback(audioDataBase.find(entity => entity.id === id));
+        // }, 300);
+
+        const xml = new XMLHttpRequest();
+
+        xml.open('GET', `http://localhost:8080/audios?id=${id}`);
+
+        xml.responseType = 'json';
+
+        xml.send();
+
+        xml.onload = () => {
+            callback(xml.response);
+        }
     }
 }
