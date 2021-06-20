a74d9bb3599293c290bafbeadc9b56132a0f177c
Serhii Serhatyi <serhatyi.serhii@gmail.com>
2021-06-20 14:57:10 +0300

    fetch in data.service methods

--- scripts/services/data.service.ts
+++ scripts/services/data.service.ts
@@ -5,40 +5,64 @@ export class DataService {
     private url = 'http://localhost:8080/';
 
     public getAllMovies(): Promise<IMovie[]> {
-        const xhr = new XMLHttpRequest(); // TODO: use fetch api
+        // const xhr = new XMLHttpRequest(); // TODO: use fetch api     Corrected
 
-        xhr.open('GET', `${this.url}movies`);
+        // xhr.open('GET', `${this.url}movies`);
 
-        xhr.responseType = 'json';
+        // xhr.responseType = 'json';
 
-        xhr.send();
+        // xhr.send();
+
+        // return new Promise<IMovie[]>(resolve => {
+        //    xhr.addEventListener('load', () => {
+        //        resolve(xhr.response);
+        //    })
+        // });
+
+        const response = fetch(`${this.url}movies`);
 
         return new Promise<IMovie[]>(resolve => {
-           xhr.addEventListener('load', () => {
-               resolve(xhr.response);
-           })
-        });
+            response.then(result => {
+                resolve(result.json());
+            })
+        })
     }
 
     public getAudioSourceById(id: number): Promise<IAudio> {
-        const xhr = new XMLHttpRequest(); // TODO: use fetch api
+        // const xhr = new XMLHttpRequest(); // TODO: use fetch api     Corrected
 
-        const json = JSON.stringify({
-            id
-        });
+        // const json = JSON.stringify({
+        //     id
+        // });
 
-        xhr.open('POST', `${this.url}audios`);
+        // xhr.open('POST', `${this.url}audios`);
 
-        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-        xhr.responseType = 'json';
+        // xhr.responseType = 'json';
 
-        xhr.send(json);
+        // xhr.send(json);
+
+        // return new Promise<IAudio>(resolve => {
+        //     xhr.addEventListener('load', () => {
+        //         resolve(xhr.response);
+        //     });
+        // })
+
+        const settings = {
+            method: 'POST',
+            body: JSON.stringify({id}),
+            headers: {
+                'Content-type': 'application/json; charset=utf-8'
+            }
+        }
+
+        const response = fetch(`${this.url}audios`, settings);
 
         return new Promise<IAudio>(resolve => {
-            xhr.addEventListener('load', () => {
-                resolve(xhr.response);
-            });
+            response.then(result => {
+                resolve(result.json());
+            })
         })
     }
 }
