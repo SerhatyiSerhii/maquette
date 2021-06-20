34894d9a871e63e8e07cf663d7d6ab91ee4b3c42
Serhii Serhatyi <serhatyi.serhii@gmail.com>
2021-06-20 17:41:59 +0300

    updated getAudio method

--- scripts/services/data.service.ts
+++ scripts/services/data.service.ts
@@ -5,34 +5,10 @@ export class DataService {
     private url = 'http://localhost:8080/';
 
     public getAllMovies(): Promise<IMovie[]> {
-        // const response = fetch(`${this.url}movies`);
-
-        // return new Promise<IMovie[]>(resolve => { // TODO: fetch returns promise itself   Corrected
-        //     response.then(result => {
-        //         resolve(result.json());
-        //     })
-        // })
-
         return fetch(`${this.url}movies`).then(response => response.json());
     }
 
     public getAudioSourceById(id: number): Promise<IAudio> {
-        const settings = {
-            method: 'POST',
-            body: JSON.stringify({id}),
-            headers: {
-                'Content-type': 'application/json; charset=utf-8'
-            }
-        }
-
-        // const response = fetch(`${this.url}audios`, settings);
-
-        // return new Promise<IAudio>(resolve => { // TODO: fetch returns promise itself    Corrected
-        //     response.then(result => {
-        //         resolve(result.json());
-        //     })
-        // })
-
-        return fetch(`${this.url}audios`, settings).then(response => response.json());
+        return fetch(`${this.url}audios/${id}`).then(response => response.json());
     }
 }
